2fe6aa19c114648350eb6dbd1638d32012df6786
Ismael Miah <ismail27.dec@gmail.com>
2024-02-28 21:30:00 +0600

    define models data for columns, status and categories

--- models/index.ts
+++ models/index.ts
@@ -7,3 +7,25 @@ export interface Product {
   sale_date: string | null;
   sale_price: number | null;
 }
+
+export const columns = [
+  { name: "IMEI", uid: "imei" },
+  { name: "MODEL", uid: "model" },
+  { name: "PRICE", uid: "price" },
+  { name: "CATEGORY", uid: "category" },
+  { name: "STATUS", uid: "status" },
+  { name: "SALE DATE", uid: "sale_date" },
+  { name: "SALE PRICE", uid: "sale_price" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+export const status = [
+  { name: "Sold", uid: "sold" },
+  { name: "Entry", uid: "entry" },
+];
+
+export const categories = [
+  { name: "Realme", uid: "realme" },
+  { name: "Xiaomi", uid: "xiaomi" },
+  { name: "Samsung", uid: "samsung" },
+];
