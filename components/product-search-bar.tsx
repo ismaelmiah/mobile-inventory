67399e9de898d791c43a8d3d7052f2e0e0601b41
Ismael Miah <ismail27.dec@gmail.com>
2024-03-02 21:30:00 +0600

    Add category and status filter searching fields in topbar

--- components/product-search-bar.tsx
+++ components/product-search-bar.tsx
@@ -2,6 +2,15 @@ import { Select, Input, SelectItem, Button } from "@nextui-org/react";
 import { SearchIcon, PlusIcon } from "@/models/Icon";
 
 export const ProductSearchBar = (props: any) => {
+  const {
+    onOpen,
+    onSearchChange,
+    phoneCategories,
+    setCategoryFilter,
+    setStatusFilter,
+    statusOptions,
+  } = props;
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-between gap-3 items-end">
@@ -11,7 +20,40 @@ export const ProductSearchBar = (props: any) => {
             placeholder="Search here..."
             startContent={<SearchIcon />}
             labelPlacement="outside"
+            onValueChange={onSearchChange}
           />
+
+          <Select
+            placeholder="Select category"
+            selectionMode="multiple"
+            labelPlacement="outside"
+            className="sm:max-w-[15%]"
+            onChange={(e) => {
+              setCategoryFilter(e.target.value);
+            }}
+          >
+            {(phoneCategories as any[]).map((item) => (
+              <SelectItem key={item.uid} value={item.uid}>
+                {item.name}
+              </SelectItem>
+            ))}
+          </Select>
+
+          <Select
+            placeholder="Status"
+            selectionMode="multiple"
+            className="sm:max-w-[10%]"
+            labelPlacement="outside"
+            onChange={(e) => {
+              setStatusFilter(e.target.value);
+            }}
+          >
+            {(statusOptions as any[]).map((status) => (
+              <SelectItem key={status.uid} value={status.uid}>
+                {status.name}
+              </SelectItem>
+            ))}
+          </Select>
 
           <div className="px-4 py-2 text-2xl text-red-900 font-semibold">
             Investment: 120434
@@ -25,6 +67,9 @@ export const ProductSearchBar = (props: any) => {
         </div>
         <div className="flex gap-3">
           <Button color="primary" 
+          onPress={() => {
+            onOpen()
+          }}
            endContent={<PlusIcon />}>
             Add New
           </Button>
