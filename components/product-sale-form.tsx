6054fbac5a6e701983f7bcce224a238523c9eb46
Ismael Miah <ismail27.dec@gmail.com>
2024-03-16 21:30:00 +0600

    form validation trigger applied

--- components/product-sale-form.tsx
+++ components/product-sale-form.tsx
@@ -86,6 +86,12 @@ export const ProductSaleForm = (props: any) => {
               </Button>
               <Button
                 color="success"
+                onPress={() => {
+                  if (isValidForm()) {
+                    onClose();
+                    handleFormSubmit();
+                  }
+                }}
               >
                 Sale
               </Button>
